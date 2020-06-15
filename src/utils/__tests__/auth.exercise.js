c6309eb41371a2e2185056697866d56b34956a05
felixjb <felixjbcomp@gmail.com>
2020-06-15 14:32:55 -0300

    Exercise: Lesson 3 - Extra 1
    Write unit tests for a simple pure function
    
    Co-authored-by: Matheus Francisco <matheusmachadoufsc@gmail.com>

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,15 +1,25 @@
 import { isPasswordAllowed } from '../auth'
 
-test('isPasswordAllowed returns true for valid password', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true);
-});
+describe('isPasswordAllowed only allows some passwords', () => {
+  const allowedPassword = ['!aBc123'];
+  const disalowedPasswords = [
+    'a2c!',
+    '123456!',
+    'ABCdef!',
+    'abc123!',
+    'ABC123!',
+    'ABCdef123',
+  ];
 
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true);
-  expect(isPasswordAllowed('a2c!')).toBe(false);
-  expect(isPasswordAllowed('123456!')).toBe(false);
-  expect(isPasswordAllowed('ABCdef!')).toBe(false);
-  expect(isPasswordAllowed('abc123!')).toBe(false);
-  expect(isPasswordAllowed('ABC123!')).toBe(false);
-  expect(isPasswordAllowed('ABCdef123')).toBe(false)
+  allowedPassword.forEach((password) => {
+    test(`allows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(true);
+    });
+  });
+
+  disalowedPasswords.forEach((password) => {
+    test(`disallows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(false);
+    });
+  });
 });
